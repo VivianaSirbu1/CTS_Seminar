75de7109ef7352ad6551b6d3d5ec5e238a731b86
Viviana <sirbuviviana18@stud.ase.ro>
2021-02-25 17:17:54 +0200

    Adaugarea claselor pt gradina zoo

--- seminar1_cts/src/program/Program.java
+++ seminar1_cts/src/program/Program.java
@@ -1,9 +1,27 @@
 package program;
 
+import clase.Giraffe;
+import clase.Zebra;
+import clase.Zoo;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Zoo z= new Zoo();
+
+		Giraffe giraffe= new Giraffe("Girafel1");
+		Giraffe giraffe2= new Giraffe("Girafel2");
+		z.addAnimal(giraffe);
+		z.addAnimal(giraffe2);
+		
+		z.feedAllAnimals();
+		
+		Zebra z1= new Zebra("z1");
+		Zebra z2= new Zebra("z2");
+		z.addAnimal(z1);
+		z.addAnimal(z2);
+		
+		z.feedAllAnimals();
 
 	}
 
