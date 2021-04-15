6183a03a0643c39f877618c956d72f29ecf72e98
VivianaSirbu1 <sirbuviviana18@stud.ase.ro>
2021-04-15 16:21:26 +0300

    Decorator

---
+++ seminar8/src/ro/ase/cts/decorator/clase/DecoratorNotaPasteFericit.java
@@ -0,0 +1,19 @@
+package ro.ase.cts.decorator.clase;
+
+public class DecoratorNotaPasteFericit extends DecoratorAbstract {
+
+	public DecoratorNotaPasteFericit(NotaAbstracta nota) {
+		super(nota);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public void printeazaFelicitare() {
+		System.out.println("Felicitare -Paste fericit");
+	}
+	
+	public void printeaza() {
+		super.printeaza();
+		System.out.println("Paste fericit!");
+	}
+}
