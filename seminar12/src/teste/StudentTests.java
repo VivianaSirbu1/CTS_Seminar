6f557159145b121967aa330556b9354291ca458c
viviana.sirbu <viviana.sirbu@itsmartsystem.eu>
2021-05-20 17:02:33 +0300

    Tests

--- seminar12/src/teste/StudentTests.java
+++ seminar12/src/teste/StudentTests.java
@@ -48,4 +48,89 @@ public class StudentTests {
 		assertEquals(1, student.getNote().size());
 
 	}
+
+	@Test
+	public void testCalculeazaMedie() {
+		Student student = new Student();
+		int nota1 = 8;
+		int nota2 = 5;
+		int nota3 = 7;
+
+		student.adaugaNota(nota1);
+		student.adaugaNota(nota2);
+		student.adaugaNota(nota3);
+		float medie = (nota1 + nota2 + nota3) / 3.0f;
+
+		assertEquals(medie, student.calculeazaMedie(), 0.01);
+
+	}
+
+	@Test
+	public void testCalculeazaMediePentruONota() {
+		Student student = new Student();
+		int nota1 = 8;
+		student.adaugaNota(nota1);
+		
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCalculeazaMediePentruZeroNota() {
+		Student student = new Student();
+		student.calculeazaMedie();
+	}
+
+	@Test
+	public void testAreRestante() {
+		Student student = new Student();
+		int nota1 = 8;
+		int nota2 = 3;
+		int nota3 = 2;
+		
+		student.adaugaNota(nota1);
+		student.adaugaNota(nota2);
+		student.adaugaNota(nota3);
+		assertTrue(student.areRestante());
+	}
+	
+
+	@Test
+	public void testNuAreRestante() {
+		Student student = new Student();
+		int nota1 = 8;
+		int nota2 = 9;
+		int nota3 = 10;
+		
+		student.adaugaNota(nota1);
+		student.adaugaNota(nota2);
+		student.adaugaNota(nota3);
+		assertFalse(student.areRestante());
+	}
+	
+	@Test
+	public void testGetNotaPozitieIncorecta() {
+		Student student = new Student();
+		int nota1 = 8;
+		int nota2 = 9;
+		int nota3 = 10;
+		
+		student.adaugaNota(nota1);
+		student.adaugaNota(nota2);
+		student.adaugaNota(nota3);
+		
+		try {
+			student.getNota(3);
+			fail("metoda nu arunca exceptie");
+		}catch(IndexOutOfBoundsException exceptie) {
+			
+		}
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testAdaugaNotIncorecta() {
+		Student student= new Student();
+		int nota=11;
+		student.adaugaNota(nota);
+	}
+	
+
 }
